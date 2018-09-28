f7c11ee51eff85e0b3371f8291d144d8eb111624
Timotei <timotei.albu@gmail.com>
2018-09-28 08:16:50 -0600

    WIp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import Grow from '@material-ui/core/Grow';
 import _ from 'lodash';
 import uuidv4 from 'uuid/v4';
 import './App.css';
@@ -16,7 +17,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      goodItems: [],
+      goodItems: [{"id":"ce49f479-7593-4f66-b512-c6160ae1945f","type":"good","body":"Please enter something"},{"id":"d04f0fc6-2a6c-4973-ad73-c354d52f018f","type":"good","body":"Please enter something"},{"id":"b48ccc30-4d34-4ada-8b25-30ab4ff94a0c","type":"good","body":"Please enter something"},{"id":"c7ac12e4-bed5-47e4-b6aa-dfe559cf6902","type":"good","body":"Please enter something"},{"id":"3cc67729-0317-4206-b20c-757dfbfdfe12","type":"good","body":"Please enter something"},{"id":"395d712d-3050-4bac-a28b-25651dc815c0","type":"good","body":"Please enter something"}],
       badItems:[],
       selected: null,
     }
@@ -72,18 +73,32 @@ class App extends Component {
           <div className="col-6">
             { _.map(goodItems, (o, i) => {
               return (
-                <div key={i} className="row">
-                  <Item { ...o } onEditClick={onEditClick}/>
-                </div>
+                <Grow
+                  key={i}
+                  in={true}
+                  timeout={1000}
+                  style={{ transformOrigin: '0 0 0' }}
+                >
+                  <div className="row" >
+                      <Item { ...o } onEditClick={onEditClick}/>
+                  </div>
+                </Grow>
               );
             })}
           </div>
           <div className="col-6">
             { _.map(badItems, (o, i) => {
               return (
-                <div key={i} className="row">
-                  <Item { ...o } onEditClick={onEditClick}/>
-                </div>
+                <Grow
+                  key={i}
+                  in={true}
+                  timeout={1000}
+                  style={{ transformOrigin: '0 0 0' }}
+                >
+                  <div key={i} className="row">
+                    <Item { ...o } onEditClick={onEditClick}/>
+                  </div>
+                </Grow>
               );
             })}
           </div>
