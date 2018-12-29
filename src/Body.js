b2c02a4e3e37862dd355e261a769594308c3d6db
Timotei Albu <timotei.albu@gmail.com>
2018-12-28 22:26:58 -0700

    Un-ejected the app. Added standard linter.

--- src/Body.js
+++ src/Body.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import Button from '@material-ui/core/Button';
